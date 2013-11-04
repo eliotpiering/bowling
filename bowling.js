03990ab027f6726eb656286da6952d6727bd4fbe
Eliot Piering <efpierin@wustl.edu>
2013-11-03 23:19:37 -0600

    bowling_game

--- bowling.js
+++ bowling.js
@@ -1,19 +1,93 @@
-function game() {
-    f = new frame();
-    this.roll = function() {
-        var rollAgain = true;
-        pins = (Math.random()*10) + 1;
-        if (pins = 10) {
-            var rollAgain = false;
+function game () {
+    this.currentFrame = new frame();
+    this.frames = [];
+    this.gameStatus = 1;
+    this.roll = function (pins) {
+        if (pins < 0 || pins > 10) {
+            throw new Error();
+        }
+        if (this.currentFrame.roll1 + pins > 10 && this.gameStatus != 10 && this.currentFrame.roll1 != 10)  {
+            throw new Error('dont cheat')
         }
 
+        if (this.currentFrame.roll1 == null) {
+            this.currentFrame.roll1 = pins
+            if (pins == 10 && this.gameStatus != 10) {
+                this.frames.push(this.currentFrame);
+                this.currentFrame = new frame();
+                this.gameStatus += 1;
+            }
+        } else if (this.currentFrame.roll2 == null) {
+            this.currentFrame.roll2 = pins
+            if (this.gameStatus < 9) {
+                this.frames.push(this.currentFrame);
+                this.currentFrame = new frame();
+                this.gameStatus += 1;
+            } else if (this.gameStatus == 9 ){
+                this.frames.push(this.currentFrame);
+                this.currentFrame = new lastFrame();
+                this.gameStatus += 1;
+            } else if (this.currentFrame.roll3 == null && (this.currentFrame.roll1 + this.currentFrame.roll2 < 10)) {
+                this.frames.push(this.currentFrame);
+                this.gameStatus = 'over';
+            }
+        } else {
+            this.currentFrame.roll3 = pins;
+            this.frames.push(this.currentFrame);
+            this.gameStatus = 'over';
+        }
     };
-    this.score = function(score) {
-    return score
-    };
+
+    this.score = function() {
+        var score = 0;
+        var lastFrameStrike = false;
+        var lastFrameSpare = false;
+        for (var i = 0; i < this.frames.length; i++) {
+            var f = this.frames[i];
+            if (lastFrameSpare) {
+                score = score + f.roll1;
+            }
+            if (lastFrameStrike){
+                score = score + f.roll1 + f.roll2;
+            }
+            if (f instanceof lastFrame && f.roll1 == 10) {
+                score = score + f.roll2 + f.roll3
+            } else if (f instanceof lastFrame && (f.roll1 + f.roll2) == 10) {
+                score += f.roll3
+            }
+
+            score += f.roll1;
+            if (f.roll2) {
+                score += f.roll2
+            }
+            if (f.roll3) {
+                score += f.roll3
+            }
+            if (f.roll1 + f.roll2 == 10 && f.roll2 != null){
+                lastFrameSpare = true;
+                lastFrameStrike = false;
+            } else if (f.roll1 == 10) {
+                lastFrameStrike = true;
+                lastFrameSpare = false;
+            } else {
+                lastFrameStrike = false;
+                lastFrameSpare = false;
+            }
+        }
+        return score
+    }
  }
 
-function frame() {
-    this.rolls = 0;
-    this.curentFrame = 0
+function frame ()
+{
+    this.roll1 = null;
+    this.roll2 = null;
 }
+
+function lastFrame ()
+{
+    this.roll1 = null;
+    this.roll2 = null;
+    this.roll3 = null;
+}
+
--- tests.js
+++ tests.js
@@ -1,17 +1,117 @@
+
 test( "see if a game, and frame exist", function() {
-    ok(game, "does it exist")
-    ok(frame, "does it exist")
+    ok(game, "does it exist");
+    ok(frame, "does it exist");
 });
 
 test("a game has ten frames", function() {
-    equal(game.frame, 10)
+    var g = new game();
+    for (var i = 0;i < 10; i++) {
+        g.roll(3);
+        g.roll(4);
+    }
+    equal(g.gameStatus, 'over', "the game should be over")
 });
 
-test("a frame can have either 1 or 2 rolls", function() {
-    equal(frame.rolls = 1 || 2)
+test("after a roll the game should update the current frame", function() {
+    var g = new game();
+    equal(g.gameStatus, 1, "it should be the 1st frame in the game");
+
+    g.roll(5);
+    g.roll(5);
+    equal(g.gameStatus, 2, "it should be the 2nd frame after not getting a strike");
+
+    g.roll(10);
+    equal(g.gameStatus, 3, "it should be the 3rd frame after getting a strike");
+
 });
 
-test("the tenth frame has either 2 or 3 rolls", function() {
-    var f = new frame(10)
-    equal(f.rolls, 2 || 3)
+
+test("if it is the tenth frame you can have up to three rolls", function() {
+    var g = new game();
+    g.gameStatus = 9;
+
+    g.roll(5);
+    g.roll(5);
+    equal(g.gameStatus, 10, "it should be the 2nd frame after not getting a strike");
+    ok(g.currentFrame instanceof lastFrame, "it should be the last frame");
+
+    g.roll(3);
+    g.roll(7);
+    equal(g.gameStatus, 10, "shouldn't be over yet");
+    g.roll(8);
+    equal(g.gameStatus, 'over',  "game over");
+
+});
+
+test("if it is the tenth frame you can have up to three rolls even if you are rolling strikes", function() {
+    var g = new game();
+    g.gameStatus = 9;
+
+    g.roll(5);
+    g.roll(5);
+    g.roll(10);
+    g.roll(10);
+    equal(g.gameStatus, 10, "shouldn't be over yet");
+    g.roll(10);
+    equal(g.gameStatus, 'over',  "game over");
+
+});
+
+test("game.roll() takes 1 number that has to be between 0 and 10", function() {
+    var g = game;
+    throws(function() {g.roll(11)},  "should throw an error");
+    throws(function() {g.roll(-1)},  "should throw an error");
+});
+
+test("you cannot get more than 10 on a single frame,", function () {
+    g = new game();
+    g.roll(6)
+    throws(function () {g.roll(7)}, "should throw an error");
+
+});
+
+test("score should add up the score of a full game", function() {
+    var g = new game();
+    for (var i = 0;i < 10; i++) {
+        g.roll(3);
+        g.roll(4);
+    }
+    equal(g.score(), 70, "you scored 74 points this game")
+});
+
+test("if you get a strike or a spare, score should take into account your next roll", function() {
+    var g = new game();
+    g.roll(5);
+    g.roll(5);
+    g.roll(2);
+    g.roll(2);
+    equal(g.score(), 16, "only the first roll after a spare should be added twice");
+
+    g.roll(0);
+    g.roll(10);
+    g.roll(10);
+    equal(g.score(), 46, "a strike should be added onto a spare");
+
+    g.roll(5);
+    g.roll(5);
+    g.roll(0);
+    g.roll(5);
+    equal(g.score(), 71, "if the next roll after a strike or spare is 0 it should not be added on");
+
+    g.roll(10);
+    g.roll(4);
+    g.roll(5);
+    equal(g.score(), 99, "the next two rolls after a strike should be added on");
+});
+
+test("score should work on the last frame", function() {
+    var g = new game();
+    g.gameStatus = 9;
+    g.roll(5);
+    g.roll(5);
+    g.roll(10);
+    g.roll(4);
+    g.roll(5);
+    equal(g.score(), 48, "the score should be 48... i think")
 });
